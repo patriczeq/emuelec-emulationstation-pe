87791468697258b407c8deb91b46c8396b57912b
Patrik Eder <stuck.eder@gmail.com>
2023-10-30 16:24:14 +0100

    neumí fast skip

--- es-core/src/components/VideoVlcComponent.cpp
+++ es-core/src/components/VideoVlcComponent.cpp
@@ -731,7 +731,7 @@ libvlc_media_player_get_position( libvlc_media_player_t *p_mi ); // vraci procen
 		{
 			newTime = total;
 		}
-		libvlc_media_player_set_time(mMediaPlayer, newTime, true);
+		libvlc_media_player_set_time(mMediaPlayer, newTime);
 	}
 
 void VideoVlcComponent::stopVideo()
