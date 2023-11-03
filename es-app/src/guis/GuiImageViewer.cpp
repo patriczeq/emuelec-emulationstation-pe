c9a540d598aa2560d0a51ecdcff04582c62a7b3b
Patrik Eder <stuck.eder@gmail.com>
2023-11-03 20:05:52 +0100

    VideoPreview stops working, topWindow?

--- es-app/src/guis/GuiImageViewer.cpp
+++ es-app/src/guis/GuiImageViewer.cpp
@@ -736,7 +736,7 @@ void GuiVideoViewer::playVideo(Window* window, const std::string videoPath, bool
 
 GuiVideoViewer::GuiVideoViewer(Window* window, const std::string& path, bool movie) : GuiComponent(window)
 {
-
+	topWindow(true);
 	vWindow = window;
 	isMovie = movie;
 	setPosition(0, 0);
