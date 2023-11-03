d7335d1b906b8bdb76f4415054d343200676be92
Patrik Eder <stuck.eder@gmail.com>
2023-11-03 10:35:19 +0100

    Update VideoVlcComponent.cpp

--- es-core/src/components/VideoVlcComponent.cpp
+++ es-core/src/components/VideoVlcComponent.cpp
@@ -582,7 +582,7 @@ void VideoVlcComponent::handleLooping()
 				libvlc_media_player_set_media(mMediaPlayer, mMedia);
 			//	libvlc_audio_set_mute(mMediaPlayer, 0);
 			loadSubtitles();
-			libvlc_set_fullscreen(mMediaPlayer);
+			libvlc_set_fullscreen(mMediaPlayer, true);
 			libvlc_media_player_play(mMediaPlayer);
 
 			AudioManager::getInstance()->VideoSetTotalTime(libvlc_media_player_get_length(mMediaPlayer));
@@ -725,7 +725,7 @@ void VideoVlcComponent::startVideo()
 				//libvlc_audio_set_mute(mMediaPlayer, 0);
 				//libvlc_audio_set_volume(mMediaPlayer, 100);
 				loadSubtitles();
-				libvlc_set_fullscreen(mMediaPlayer);
+				libvlc_set_fullscreen(mMediaPlayer, true);
 				libvlc_media_player_play(mMediaPlayer);
 				//loadSubtitles();
 				AudioManager::getInstance()->VideoSetTotalTime(libvlc_media_player_get_length(mMediaPlayer));
