e1ba38a74175ac57f28845164e3db1ca531be7f5
Patrik Eder <stuck.eder@gmail.com>
2023-11-15 12:13:07 +0100

    Update GuiMenu.cpp

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -932,7 +932,7 @@ void GuiMenu::openSTAmenu(std::vector<std::string> stations)
 
 
 							std::string _title 	=  _mac + " -> " + _ssid;
-							std::string _subtitle 	=  _vendor + " -> " + _bssid + (_channel.empty() ? "" : (" CH" + channel);
+							std::string _subtitle 	=  _vendor + " -> " + _bssid + (_channel.empty() ? "" : (" CH" + _channel);
 							//inline void addWithDescription(const std::string& label, const std::string& description, const std::shared_ptr<GuiComponent>& comp, const std::function<void()>& func, const std::string iconName = "", bool setCursorHere = false, /*bool invert_when_selected = true,*/ bool multiLine = false)
 
 							s->addWithDescription(_title, _subtitle,
