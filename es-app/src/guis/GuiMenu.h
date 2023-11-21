0725bf574295e583f1653a90f12cf6ccc723de86
Patrik Eder <stuck.eder@gmail.com>
2023-11-21 14:10:31 +0100

    Update GuiMenu.h

--- es-app/src/guis/GuiMenu.h
+++ es-app/src/guis/GuiMenu.h
@@ -521,7 +521,7 @@ public:
 	static void loadChromecast(Window* mWindow, std::string file);
 	static void loadChromecastDevices(Window* mWindow, std::vector<AVAHIserviceDetail> casts, std::string file);
 	static void loadChromecastDevice(Window* mWindow, Chromecast device, std::string file);
-
+	static std::vector<AVAHIserviceDetail> getAvahiService(std::string service);
 private:
 	void addEntry(std::string name, bool add_arrow, const std::function<void()>& func, const std::string iconName = "");
 
@@ -547,7 +547,7 @@ private:
 	std::vector<AVAHIservice> getAvahiServices();
 	void openAvahiList(std::vector<AVAHIservice> list);
 
-	std::vector<AVAHIserviceDetail> getAvahiService(std::string service);
+
 	void openAvahiDetailList(std::vector<AVAHIserviceDetail> list);
 	void openAvahiDetail(AVAHIserviceDetail service);
 
