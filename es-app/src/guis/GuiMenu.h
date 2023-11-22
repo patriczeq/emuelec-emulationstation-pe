6615a02754ac2d320bf8c6bdfc52ae423c42446e
Patrik Eder <stuck.eder@gmail.com>
2023-11-22 20:52:02 +0100

    Update GuiMenu.h

--- es-app/src/guis/GuiMenu.h
+++ es-app/src/guis/GuiMenu.h
@@ -519,9 +519,9 @@ public:
 	static void editKeyboardMappings(Window *window, IKeyboardMapContainer* mapping, bool editable);
 
 	static void loadChromecast(Window* mWindow, std::string file = "");
-	static void loadChromecastDevices(Window* mWindow, std::vector<AVAHIserviceDetail> casts, std::string file);
-	static void loadChromecastDevice(Window* mWindow, Chromecast device, std::string file);
-	static void castFile(Chromecast device, std::string file);
+	static void loadChromecastDevices(Window* mWindow, std::vector<AVAHIserviceDetail> casts, std::string file = "");
+	static void loadChromecastDevice(Window* mWindow, Chromecast device, std::string file = "");
+	static void castFile(Chromecast device, std::string file = "");
 	static std::vector<AVAHIserviceDetail> getAvahiService(std::string service);
 private:
 	void addEntry(std::string name, bool add_arrow, const std::function<void()>& func, const std::string iconName = "");
