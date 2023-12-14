2424e9e19fb7e510df6acd46c7344d34363fa799
Patrik Eder <stuck.eder@gmail.com>
2023-12-14 10:39:42 +0100

    Update GuiMenu.cpp

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -722,7 +722,7 @@ void GuiMenu::openESP01Settings()
 			nBright->setValue(Settings::getInstance()->getInt("pe_hack.neobright"));
 			nBright->setOnValueChanged([](const float &newVal) {
 				Settings::getInstance()->setInt("pe_hack.neobright", (int)round(newVal));
-				hacksSend("bright " + std::to_string(value));
+				hacksSend("bright " + std::to_string((int)round(newVal)));
 			});
 			s->addWithLabel(_("BRIGHTNESS"), nBright);
 
